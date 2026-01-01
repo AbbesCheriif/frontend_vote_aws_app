0067aaf77b3de081860e56d1eb9cc478c032f3e6
abbescheriif <abbes.cherif@enis.tn>
2026-01-01 14:10:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  standalone: true
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  projects: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:5000/projects')
+      .subscribe((data: any) => this.projects = data);
+  }
+
+  vote(projectId: number) {
+    this.http.post('http://VOTING-GATEWAY-IP:5000/vote', {
+      project_id: projectId
+    }).subscribe(() => alert('Vote enregistré'));
+  }
 }
+
+
+
+
+
+// version originale (aucune modification)
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'frontend';
+// }
